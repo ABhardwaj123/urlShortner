13397e08059bdc41fee08a593d6a3dd575afda89
ABhardwaj123 <agastya.bhardwajdps@gmail.com>
2026-08-12 19:52:46 +0530

    adding foreign key of user

--- backend/src/models/url.models.js
+++ backend/src/models/url.models.js
@@ -2,6 +2,12 @@ import mongoose , {Schema} from "mongoose";
 
 const urlSchema = new Schema({
 
+        userId:{
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: false
+        },
+
         url: {
             type: String,
             required: true,
