d2d07aa4d34af6a368b227ba89c91af55f1ed89a
ABhardwaj123 <agastya.bhardwajdps@gmail.com>
2026-08-12 18:39:50 +0530

    adding routers of url and auth

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,13 @@
 import express from "express"
+import urlRoutes from './routes/urlRoutes.js'
+import { getRedirect } from "./controllers/urlController.js"
+import authRoutes from './routes/authRoutes.js'
 
 const app = express()
+app.use('/shorten' , urlRoutes)
+app.use('/auth' , authRoutes)
+app.get('/:shortCode', getRedirect)
+
 
 app.get('/' , (req , res) => {
     res.send('server is running')
