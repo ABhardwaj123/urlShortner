42723c419e588854af3eb2c87429c6a28c9996c8
ABhardwaj123 <agastya.bhardwajdps@gmail.com>
2026-08-13 22:04:03 +0530

    adding cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,14 @@
 import express from "express"
+import cors from 'cors'
 import urlRoutes from './routes/urlRoutes.js'
 import { getRedirect } from "./controllers/urlController.js"
 import authRoutes from './routes/authRoutes.js'
 
 const app = express()
+
+app.use(cors())
+app.use(express.json())
+
 app.use('/shorten' , urlRoutes)
 app.use('/auth' , authRoutes)
 app.get('/:shortCode', getRedirect)
