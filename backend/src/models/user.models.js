72db0b7b176c5c1161d200a2f51cf40124ea602a
ABhardwaj123 <agastya.bhardwajdps@gmail.com>
2026-08-14 19:50:21 +0530

    fixing syntax error

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -21,11 +21,10 @@ const userSchema = new Schema({
 //this is a mongoose pre-save hook. automatically hash a user's password before the user document is saved
 //'this' cannot be used with =>. function has to be used explicitly
 userSchema.pre('save', async function (next) {
-    if(!this.isModified('password')) return next()
+    if(!this.isModified('password')) return
 
     //10 is salt rounds or cost factor
     this.password = await bcrypt.hash(this.password , 10)
-    next()
 })
 
 
