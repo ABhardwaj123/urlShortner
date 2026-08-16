061e233fbee54fc4bea5c7e1b1e412f28ba44661
ABhardwaj123 <agastya.bhardwajdps@gmail.com>
2026-08-16 19:33:11 +0530

    tightening cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ import authRoutes from './routes/authRoutes.js'
 
 const app = express()
 
-app.use(cors())
+app.use(cors({ origin: 'https://grand-narwhal-78cd0d.netlify.app' }))
 app.use(express.json())
 
 app.use('/shorten' , urlRoutes)
